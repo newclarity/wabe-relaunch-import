b4ade7642d2532e593895882149b02233e1252cd
Daryl Lozupone <daryl@newclarity.net>
2017-08-14 12:51:51 -0400

    Add path to terminus executable

--- workflow/terminus.sh
+++ workflow/terminus.sh
@@ -6,7 +6,7 @@
 curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar && php installer.phar install
 
 # Authenticate to Pantheon
-terminus auth:login --machine-token=${PANTHEON_MACHINE_TOKEN}
+./vendor/bin/terminus auth:login --machine-token=${PANTHEON_MACHINE_TOKEN}
 
 # Clone the live database to the target environment
-terminus env:clone-content --db-only ${PANTHEON_SITE_NAME}.live preview
+./vendor/bin/terminus env:clone-content --db-only ${PANTHEON_SITE_NAME}.live preview
