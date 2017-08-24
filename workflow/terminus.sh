67117dce81fe7f107c514662f236536c7e2cc28f
Daryl Lozupone <daryl@newclarity.net>
2017-08-24 13:58:56 -0400

    Add import branch

--- workflow/terminus.sh
+++ workflow/terminus.sh
@@ -2,6 +2,8 @@
 
 # @link https://pantheon.io/docs/terminus
 
+source shared/scripts.sh
+
 # Install Terminus
 announce "Installing Terminus"
 curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar && php installer.phar install
