afdbb839ed29cbb637a5b1602cbfe1ac466c9667
Daryl Lozupone <daryl@newclarity.net>
2017-08-24 15:19:35 -0400

    Add full path to scripts

--- workflow/import.sh
+++ workflow/import.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-source shared/scripts.sh
-
 TARGET_HOSTNAME=dbserver.import.d5981687-a2b6-4a2c-9456-ae933c90b097.drush.in
 DB_USERNAME=pantheon
 DB_PASSWORD=ef4d5a6cfe564c0c84b86bfb7013d9d6
 DB_PORT=11409
 DB_NAME=pantheon
 
+declare=${PANTHEON_SITE_NAME:=}
+declare=${WORKFLOW_ROOT:=}
+
 # Clone the live database and files to the target environment
 announce "Cloning WABE live site database to import branch"
 ./vendor/bin/terminus env:clone-content -y ${PANTHEON_SITE_NAME}.live import
 
-source includes/import_old_data.sh
+source ${WORKFLOW_ROOT}/includes/import_old_data.sh
