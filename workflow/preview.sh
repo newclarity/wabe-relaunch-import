c9e3c8abb4aee3cc2f8f71b4f34117aa084d9871
Daryl Lozupone <daryl@newclarity.net>
2017-09-06 14:40:44 -0400

    Update preview environment variables

--- workflow/preview.sh
+++ workflow/preview.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-source shared/scripts.sh
+declare=${PANTHEON_SITE_NAME:=}
+declare=${WORKFLOW_ROOT:=}
 
-TARGET_HOSTNAME=dbserver.preview.d5981687-a2b6-4a2c-9456-ae933c90b097.drush.in
+source ${WORKFLOW_ROUTE}/shared/scripts.sh
+
+DB_HOSTNAME=dbserver.preview.d5981687-a2b6-4a2c-9456-ae933c90b097.drush.in
 DB_USERNAME=pantheon
 DB_PASSWORD=5d2c4707db8b42f6900ad610d74ad362
 DB_PORT=16159
+DB_NAME=pantheon
+WWW_HOST=http://preview-wabe.pantheonsite.io
 
 # Clone the live database and files to the target environment
 announce "Cloning WABE live site database and files to preview branch"
-./vendor/bin/terminus env:clone-content -y ${PANTHEON_SITE_NAME}.live preview
+./vendor/bin/terminus env:clone-content --db-only -y ${PANTHEON_SITE_NAME}.live preview
 
-source includes/import_old_data.sh
+source ${WORKFLOW_ROOT}/includes/import_old_data.sh
