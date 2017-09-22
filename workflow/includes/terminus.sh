d39ae267360d2744d93b5edd6fd0a8ee8fc0d726
Mike Schinkel <mike@newclarity.net>
2017-09-21 22:16:51 -0400

    Fixed terminus by just hardcoding the Pantheon Machine Token

--- workflow/includes/terminus.sh
+++ workflow/includes/terminus.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+declare=${PANTHEON_MACHINE_TOKEN:=}
+
 # @link https://pantheon.io/docs/terminus
 
 cd ~/
@@ -13,7 +15,7 @@ if ! [ -d terminus ]; then
 
     # Authenticate to Pantheon
     announce "Authenticating with Pantheon via machine token"
-    execute_terminus auth:login --machine-token="$(pantheon_machine_token)"
+    execute_terminus auth:login --machine-token="${PANTHEON_MACHINE_TOKEN}"
 
 fi
 announce "Setting alias to Pantheon's Terminus CLI"
