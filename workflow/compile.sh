03f294dd40fcd5af7fad719f8fa170bb1dd23da4
Mike Schinkel <mike@newclarity.net>
2017-09-23 22:41:35 -0400

    Damn last trailing comma.  Why can't SQL be like PHP array initializers?

--- workflow/compile.sh
+++ workflow/compile.sh
@@ -108,22 +108,17 @@ if [ "yes" = "${REGEN_MENU_IMPORTS}" ]; then
 
 fi
 
-function generate_menu_insert_sql() {
-    if [ "yes" = "${REGEN_MENU_IMPORTS}" ]; then 
-        echo "SELECT * FROM $1 UNION"
-    fi 
-}
-
 #
 # Add post types that are primarily sourced on 'preview'
 #
 
 announce "...Generating new_posts from wp_posts on 'preview'"
 post_types="$(quote_mysql_set "${POST_TYPES}")"
-menu_insert_sql="$(generate_menu_insert_sql "new_menu_items")"
 execute_mysql "DROP TABLE IF EXISTS new_posts;
     CREATE TABLE new_posts LIKE wp_posts;
-    INSERT INTO new_posts ${menu_insert_sql}
+    INSERT INTO new_posts
+    SELECT * FROM new_menu_items
+    UNION
     SELECT * FROM wp_posts WHERE 1=1
         AND post_status IN ('publish','private','draft','revision','inherit')
         AND post_type IN (${post_types});"
@@ -136,18 +131,20 @@ execute_mysql "INSERT INTO new_posts
 
 announce "...Generating new_postmeta from wp_postmeta on 'preview'"
 meta_keys="$(quote_mysql_set "${META_KEYS}")"
-menu_insert_sql="$(generate_menu_insert_sql "new_menu_item_meta")"
 execute_mysql "DROP TABLE IF EXISTS new_postmeta;
     CREATE TABLE new_postmeta LIKE wp_postmeta;
-    INSERT INTO new_postmeta ${menu_insert_sql}
+    INSERT INTO new_postmeta
+    SELECT * FROM new_menu_item_meta
+    UNION
     SELECT * FROM wp_postmeta WHERE post_id IN (SELECT ID FROM new_posts) OR meta_key IN ( ${meta_keys} );"
 
 announce "...Generating new_terms from wp_terms on 'preview'"
 taxonomies="$(quote_mysql_set "${TAXONOMIES}")"
-menu_insert_sql="$(generate_menu_insert_sql "new_menus")"
 execute_mysql "DROP TABLE IF EXISTS new_terms;
     CREATE TABLE new_terms LIKE wp_terms;
-    INSERT INTO new_terms ${menu_insert_sql}
+    INSERT INTO new_terms
+    SELECT * FROM new_menus
+    UNION
     SELECT * FROM wp_terms WHERE 1=0
         OR term_id >= ${STARTING_TERM_ID}
         OR term_id IN (
@@ -155,19 +152,21 @@ execute_mysql "DROP TABLE IF EXISTS new_terms;
     );"
 
 announce "...Generating new_term_taxonomy from wp_term_taxonomy on 'preview'"
-menu_insert_sql="$(generate_menu_insert_sql "new_menu_taxonomy")"
 execute_mysql "DROP TABLE IF EXISTS new_term_taxonomy;
     CREATE TABLE new_term_taxonomy LIKE wp_term_taxonomy;
-    INSERT INTO new_term_taxonomy ${menu_insert_sql}
+    INSERT INTO new_term_taxonomy
+    SELECT * FROM new_menu_taxonomy
+    UNION
     SELECT * FROM wp_term_taxonomy WHERE 1=0
         OR term_taxonomy_id>=${STARTING_TERM_ID}
         OR term_id IN (SELECT term_id FROM new_terms);"
 
 announce "...Generating new_term_relationships table from wp_term_relationships on 'preview'"
-menu_insert_sql="$(generate_menu_insert_sql "new_menu_relationships")"
 execute_mysql "DROP TABLE IF EXISTS new_term_relationships;
     CREATE TABLE new_term_relationships LIKE wp_term_relationships;
-    INSERT INTO new_term_relationships ${menu_insert_sql}
+    INSERT INTO new_term_relationships
+    SELECT * FROM new_menu_relationships
+    UNION
     SELECT * FROM wp_term_relationships WHERE 1=0
         OR term_taxonomy_id>=${STARTING_TERM_ID}
         OR object_id IN (SELECT ID FROM new_posts)
@@ -194,7 +193,7 @@ execute_mysql "DROP TABLE IF EXISTS new_options;
         'page_for_posts',
         'page_on_front',
         'current_theme',
-        'theme_mods_wabe-theme',
+        'theme_mods_wabe-theme'
     );"
 
 # Add Home page configs? OR is this referring to setting the WP "Reading Settings First Page Displays" option?
